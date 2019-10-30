865b28c4b1d9c56e73feb808fab601db211f88a1
thomas hastings <thomas@thomashastings.com>
2019-10-30 15:56:22 -0600

    updated about me link

--- admin/assets/js/admin.js
+++ admin/assets/js/admin.js
@@ -1,9 +1,9 @@
 //get username and password
 $("#loginBtn").click(function () {
-  $( "#alert" ).html("");
-  let username = $( "#inputUsername" ).val();
-  let password = $( "#inputPassword" ).val();
-  let loginObj = '{ "Username": "'+username+'", "Password": "'+password+'" }';
+  $("#alert").html("");
+  let username = $("#inputUsername").val();
+  let password = $("#inputPassword").val();
+  let loginObj = '{ "Username": "' + username + '", "Password": "' + password + '" }';
   getToken(loginObj, username);
 });
 
@@ -18,16 +18,25 @@ function getToken(formData, username) {
     success: function (data, status, xhr) {
       getTokenSuccess(data.Message, username)
     },
-    error: function(data) {getTokenError(data.responseText)}
+    error: function (data) { getTokenError(data.responseText) }
   });
 }
 
 function getTokenError(errorMsg) {
-  $( "#alert" ).html("<p>Error: "+errorMsg+"</p>");
+  $("#alert").html("<p>Error: " + errorMsg + "</p>");
 }
 
 function getTokenSuccess(token, username) {
   setCookie("username", username, 5);
   setCookie("token", token, 5);
+  var simplemde = new SimpleMDE({
+    autosave: {
+      enabled: true,
+      uniqueId: "HastingsBlog",
+      delay: 1000,
+    },
+    element: document.getElementById("markdown")
+  });
   $("#login").hide();
+  $("#newPost").show();
 }
