ed4046e7156a5b40a51a5d8a9ed689159d3db225
thomas hastings <thomas@thomashastings.com>
2019-10-30 13:21:08 -0600

    working on auth

--- admin/assets/js/admin.js
+++ admin/assets/js/admin.js
@@ -2,7 +2,7 @@
 $("#loginBtn").click(function () {
   let username = $( "#inputUsername" ).val();
   let password = $( "#inputPassword" ).val();
-  let loginObj = JSON.parse('{ "Username": "'+username+'", "Password": "'+password+'" }');
+  let loginObj = '{ "Username": "'+username+'", "Password": "'+password+'" }';
   console.log(loginObj);
   login(loginObj);
 });
@@ -12,8 +12,8 @@ function login(formData) {
     type: 'POST',
     url: "https://api.ascode.io/auth",
     data: formData,
-    dataType: "text",
-    contentType: "application/json",
+    dataType: "json",
+    contentType: "text/plain",
     success: function (data) { console.log(data); },
     failure: function(data) {console.log(data);}
   });
