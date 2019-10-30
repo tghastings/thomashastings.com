d26f9a7228d3aac2c941b11e9fda6f0f7543f198
thomas hastings <thomas@thomashastings.com>
2019-10-30 15:18:30 -0600

    added ajax for error

--- admin/assets/js/admin.js
+++ admin/assets/js/admin.js
@@ -18,6 +18,10 @@ function getToken(formData) {
     success: function (data, status, xhr) { 
       setCookie("token", data.Message, 5);
     },
-    error: function(data) {console.log("error");}
+    error: function(data) {getTokenError(data.responseText)}
   });
+}
+
+function getTokenError(errorMsg) {
+  $( "#alert" ).html("<p>Error: "+errorMsg+"</p>");
 }
