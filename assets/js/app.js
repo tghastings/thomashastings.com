10438be07cdae3d2cdd8c70f686ec3624fa6c75e
thomas hastings <thomas@thomashastings.com>
2019-11-11 13:36:55 -0700

    Updated to test get token

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,37 +28,6 @@ function includeHTML() {
   }
 }
 
-function setCookie(name,value,days) {
-  var expires = "";
-  if (days) {
-      var date = new Date();
-      date.setTime(date.getTime() + (days*24*60*60*1000));
-      expires = "; expires=" + date.toUTCString();
-  }
-  document.cookie = name + "=" + (value || "")  + expires + "; SameSite=None; Secure; path=/";
-}
-
-function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i <ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-};
-
-function checkCookie() {
-  var username = getCookie("Token");
-  console.log(username)
-};
-
 function main() {
   includeHTML();
 }
